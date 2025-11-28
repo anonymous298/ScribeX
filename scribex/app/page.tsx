867e0a90e60bc3b390a9f9eb694285f2e29c1738
anonymous298 <tackletalha@gmail.com>
2025-11-28 09:27:09 +0500

    changes

--- scribex/app/page.tsx
+++ scribex/app/page.tsx
@@ -1,4 +1,7 @@
+"use client"
+
 import MainHeroPage from "@/components/HomePage/MainHeroPage";
+import { motion } from "motion/react";
 import Image from "next/image";
 
 export default function Home() {
@@ -6,6 +9,7 @@ export default function Home() {
   return (
     <div className="max-w-7xl mx-auto">
       <MainHeroPage/>
+
     </div>
   );
 }
