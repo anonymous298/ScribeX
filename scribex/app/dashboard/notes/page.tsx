d5c7558f0201f2059e5c8a81be57afa41a5bf112
anonymous298 <tackletalha@gmail.com>
2025-11-29 09:07:41 +0500

    dashboard/notes route skeleton changes enhanced

--- scribex/app/dashboard/notes/page.tsx
+++ scribex/app/dashboard/notes/page.tsx
@@ -1,42 +1,14 @@
 "use client"
 
 import { ExpandableCardDemo } from '@/components/dashboard/NoteCardComponent';
-import { NoteCardSkeleton } from '@/components/dashboard/NoteCardSkeleton';
-// import SpotlightCard from '@/components/SpotlightCard';
-import { getAllNotes } from '@/server/actions/note.action'
 import React, { useEffect, useState } from 'react'
 
-type Note = {
-  id: string;
-  author_id: string;
-  title: string;
-  content: string | null;
-  createdAt: Date;
-  updatedAt: Date;
-};
 
 const Page = () => {
-
-  const [notes, setNotes] = useState<Note[]>([])
-
-   // Fetch notes
-    useEffect(() => {
-      const fetchNotes = async () => {
-        const allNotes = await getAllNotes();
-        setNotes(allNotes ?? []);
-      };
-      fetchNotes();
-    }, []);
-
+  
   return (
     <div>
-      {!!notes.length ? (
-        <ExpandableCardDemo/>
-      )
-      :
-      (
-        <NoteCardSkeleton/>
-      )}
+      <ExpandableCardDemo/>
     </div>
   )
 }
